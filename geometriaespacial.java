3dea9b817d138b2fa7f16faa18d06b013ba206af
camile <85092924004@senacrs.edu.br>
2025-08-27 10:39:24 -0300

    objetos feitos

--- geometriaespacial.java
+++ geometriaespacial.java
@@ -15,24 +15,32 @@ public class geometriaespacial extends aplicacao {
       System.out.printf("Qual figura você quer?");
       formaespacial = ler.nextInt();
 
+      // NomeDaClasse NomeDoObjeto = new NomeDoConstrutor();
+      geometriaespacial objesp = new geometriaespacial();
       switch (formaespacial){
-        case 1:     
-        cilindro();
+        case 1:    
+        
+        objesp.cilindro();
         break;
 
         case 2:
-        piramide()
+        objesp.piramide();
         break;
 
         case 3:
+        objesp.esfera();
+        break;
 
         case 4:
+        objesp.cone();
+        break;
 
-        case 5: 
-
-        case 6:
+        case 5:
+        objesp.tronco();
+        break;
       }
     }
+
 
     //CILINDRO
       private void cilindro() {
@@ -64,6 +72,7 @@ public class geometriaespacial extends aplicacao {
             System.out.printf("Volume:" +volumec,"π");
             }
         }
+
 
         //PIRAMIDE
         private void piramide() {
@@ -98,6 +107,93 @@ public class geometriaespacial extends aplicacao {
             else if (escolhapiramide == 3) {
             int volumep = (areadabasep * hpiramide) / 3;
             System.out.printf("Volume:" +volumep);
+            }
+        }
+
+
+        //ESFERA
+        private void esfera() {
+        Scanner ler = new Scanner(System.in);
+
+        int resfera;
+        System.out.printf("Qual o RAIO da esfera?");
+        resfera = ler.nextInt();
+
+        System.out.println("1) Área superficial");
+        System.out.println("2) Volume");
+        System.out.println("O que tu quer calcular?");
+        int escolhaesfera = ler.nextInt();
+
+            if (escolhaesfera == 1) {
+                int areasuperficiale = 4 * (resfera ^ 2);
+                System.out.printf("Área das bases:" +areasuperficiale,"π");
+            }
+            else if (escolhaesfera == 2) {
+                int volumee = (4 / 3) * (resfera ^ 3);
+                System.out.printf("Área lateral:" +volumee,"π");
+            }
+        }
+
+
+        //CONE
+        private void cone() {
+        Scanner ler = new Scanner(System.in);
+
+        int rcone;
+        int gcone;
+        int hcone;
+        System.out.printf("Qual o RAIO do cone?");
+        rcone = ler.nextInt();
+        System.out.printf("Qual a GERATRIZ do cone?");
+        gcone = ler.nextInt();
+        System.out.printf("Qual a ALTURA do cone?");
+        hcone = ler.nextInt();
+
+        System.out.println("1) Área lateral");
+        System.out.println("2) Área da base");
+        System.out.println("2) Volume");
+        System.out.println("O que tu quer calcular?");
+        int escolhacone = ler.nextInt();
+
+            if (escolhacone == 1) {
+                int arealateralc = rcone * gcone ;
+                System.out.printf("Área lateral:" +arealateralc,"π");
+            }
+            else if (escolhacone == 2) {
+                int areadabasec = rcone ^ 2;
+                System.out.printf("Área da base:" +areadabasec,"π");
+            }
+
+            else if (escolhacone == 3) {
+                int volumeco = (rcone * hcone) / 3;
+                System.out.printf("Volume:" +volumeco,"π");
+            }
+        }
+
+        //TRONCO
+        private void tronco() {
+        Scanner ler = new Scanner(System.in);
+
+        int rpequenotronco;
+        int rgrandetronco;
+        int htronco;
+        System.out.printf("Qual o RAIO PEQUENOS do tronco?");
+        rpequenotronco = ler.nextInt();
+        System.out.printf("Qual o RAIO GRANDE do tronco?");
+        rgrandetronco = ler.nextInt();
+        System.out.printf("Qual a ALTURA do tronco?");
+        htronco = ler.nextInt();
+
+        System.out.println("1) Volume");
+        System.out.println("O que tu quer calcular?");
+        int escolhatronco = ler.nextInt();
+
+            if (escolhatronco == 1) {
+                int volumet = (htronco * ((rpequenotronco + (rpequenotronco ^ 2)) * (rgrandetronco + (rgrandetronco ^ 2)))) / 3 ;
+                System.out.printf("Volume:" +volumet,"π");
+            }
+            else if (escolhatronco == 2) {
+                System.out.printf("não tem...");
             }
         }
     }
